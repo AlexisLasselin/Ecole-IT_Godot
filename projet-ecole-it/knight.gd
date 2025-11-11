d83344390754235980d4f5b1c7eaa442911d1aab
Alexis Lasselin <114481578+AlexisLasselin@users.noreply.github.com>
2025-11-11 14:39:36 +0100

    Add initial Godot web game and assets
    Added all necessary files for the Godot web export, including HTML, JS, WASM, icons, and game assets. Also introduced new Godot scene and resources for the 'knight' character and updated project configuration and level_1 scene to integrate these assets.

---
+++ projet-ecole-it/knight.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+const SPEED = 200.0
+const JUMP_VELOCITY = -300.0
+@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	if (velocity.x > 1 or velocity.x < -1):
+		animation.animation = "run"
+	else:
+		animation.animation = "idle"
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED/3)
+
+	move_and_slide()
+	
+	if (velocity.x < 0):
+		animation.flip_h = true	
+	else:
+		animation.flip_h = false
